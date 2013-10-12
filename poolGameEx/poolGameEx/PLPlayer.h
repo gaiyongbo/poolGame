1de220a2f08bae564d8bb2fdcb097e726cf64ee3
郭 健 <guojianyizhong@163.com>
2013-10-12 14:02:29 +0800

    no message

---
+++ poolGameEx/poolGameEx/PLPlayer.h
@@ -0,0 +1,22 @@
+//
+//  PLPlayer.h
+//  poolGameEx
+//
+//  Created by 郭 健 on 13-10-12.
+//  Copyright (c) 2013年 ggvggv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+typedef enum
+{
+    PLPlayerType0 = 0,
+    PLPlayerType1,
+    PLPlayerType2,
+    PLPlayerType3
+}PLPlayerType;
+@interface PLPlayer : NSObject
+
+@property(nonatomic)PLPlayerType            mType;
+@property(nonatomic)NSInteger               mScore;
+@property(nonatomic,retain)NSMutableArray   *mBalls;
+@end
