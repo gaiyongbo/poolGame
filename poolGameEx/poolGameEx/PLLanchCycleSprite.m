3445e475b934085711fd295adcf3d4abc65e11da
zhangyaru <mylookee@hotmail.com>
2013-10-16 18:49:45 +0800

    客户版本

--- poolGameEx/poolGameEx/PLLanchCycleSprite.m
+++ poolGameEx/poolGameEx/PLLanchCycleSprite.m
@@ -8,7 +8,7 @@
 
 #import "PLLanchCycleSprite.h"
 
-#define PL_LANCH_FORCE_DELTA        -0.085
+#define PL_LANCH_FORCE_DELTA        -0.24
 @implementation PLLanchCycleSprite
 
 +(id)LanchCycleSprite
